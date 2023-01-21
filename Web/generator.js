55a94cc149e3e8b81fd1176d59aaa485e25e78be
Martin Mužík <100752257+MartinMuzik@users.noreply.github.com>
2023-01-21 18:01:33 +0100

    Add files via upload

--- Web/generator.js
+++ Web/generator.js
@@ -3,6 +3,8 @@ const SONG_NAME_INPUT = document.getElementById("song-name-input");
 const TEMPO_INPUT = document.getElementById("tempo-input");
 const OUTPUT_TEXTAREA = document.getElementById("output-textarea");
 const SAVE_METADATA_BUTTON = document.getElementById("save-metadata-btn");
+const PLAY_HARMONY_BUTTON = document.getElementById("play-harmony-btn");
+const RESET_HARMONY_BUTTON = document.getElementById("reset-harmony-btn");
 const STRING_ONE_SELECTOR = document.getElementById("string-one-selector");
 const STRING_TWO_SELECTOR = document.getElementById("string-two-selector");
 const STRING_THREE_SELECTOR = document.getElementById("string-three-selector");
@@ -34,6 +36,54 @@ SAVE_METADATA_BUTTON.addEventListener("click", function() {
   OUTPUT_TEXTAREA.innerHTML = output.replace("\n", "&#10");
 });
 
+// play current harmony
+PLAY_HARMONY_BUTTON.addEventListener("click", function() {
+  if(TEMPO_INPUT.value >= 40 && TEMPO_INPUT.value <= 240) {
+    let length = ((60/TEMPO_INPUT.value)/(NOTE_LENGTH_SELECTOR.value/4))
+    let audios = [];
+
+    if (STRING_ONE_SELECTOR.value != "X") {
+        audios.push(new Audio(`/sounds/1_${STRING_ONE_SELECTOR.value}.wav`));
+    }
+    if (STRING_TWO_SELECTOR.value != "X") {
+      audios.push(new Audio(`/sounds/2_${STRING_TWO_SELECTOR.value}.wav`));
+    }
+    if (STRING_THREE_SELECTOR.value != "X") {
+      audios.push(new Audio(`/sounds/3_${STRING_THREE_SELECTOR.value}.wav`));
+    }
+    if (STRING_FOUR_SELECTOR.value != "X") {
+      audios.push(new Audio(`/sounds/4_${STRING_FOUR_SELECTOR.value}.wav`));
+    }
+    if (STRING_FIVE_SELECTOR.value != "X") {
+      audios.push(new Audio(`/sounds/5_${STRING_FIVE_SELECTOR.value}.wav`));
+    }
+    if (STRING_SIX_SELECTOR.value != "X") {
+      audios.push(new Audio(`/sounds/6_${STRING_SIX_SELECTOR.value}.wav`));
+    }
+
+    audios.forEach(audio => {
+      audio.play();
+    });
+    setTimeout(function(){
+      audios.forEach(audio => {
+        audio.pause();
+        audio.currentTime = 0;
+      });
+    }, length*1000);
+  } else {
+    console.log("Tempo out of range.");
+  }
+});
+
+RESET_HARMONY_BUTTON.addEventListener("click", function() {
+  STRING_ONE_SELECTOR.value = "X";
+  STRING_TWO_SELECTOR.value = "X";
+  STRING_THREE_SELECTOR.value = "X";
+  STRING_FOUR_SELECTOR.value = "X";
+  STRING_FIVE_SELECTOR.value = "X";
+  STRING_SIX_SELECTOR.value = "X";
+});
+
 // play note and update string selector
 function selectNote(note) {
   //debug only
@@ -48,27 +98,27 @@ function selectNote(note) {
 
   switch (note.substring(0, 1)) {
     case "1":
-        STRING_ONE_SELECTOR.value = note.substring(2);
-        console.log(STRING_ONE_SELECTOR.value);
-        break;
+      STRING_ONE_SELECTOR.value = note.substring(2);
+      console.log(STRING_ONE_SELECTOR.value);
+      break;
     case "2":
-        STRING_TWO_SELECTOR.value = note.substring(2);
-        break;
+      STRING_TWO_SELECTOR.value = note.substring(2);
+      break;
     case "3":
-        STRING_THREE_SELECTOR.value = note.substring(2);
-        break;
+      STRING_THREE_SELECTOR.value = note.substring(2);
+      break;
     case "4":
-        STRING_FOUR_SELECTOR.value = note.substring(2);
-        break;
+      STRING_FOUR_SELECTOR.value = note.substring(2);
+      break;
     case "5":
-        STRING_FIVE_SELECTOR.value = note.substring(2);
-        break;
+      STRING_FIVE_SELECTOR.value = note.substring(2);
+      break;
     case "6":
-        STRING_SIX_SELECTOR.value = note.substring(2);
-        break;
+      STRING_SIX_SELECTOR.value = note.substring(2);
+      break;
   }
 }
 
-function addHarmony() {
+function playHarmony() {
     
 }
