e94763fb4843538b3a83cb321d2d6cf69ffea6c0
Martin Mužík <100752257+MartinMuzik@users.noreply.github.com>
2023-01-22 19:38:30 +0100

    Added measureManager, tab download, new song

--- Web/generator.js
+++ Web/generator.js
@@ -2,11 +2,14 @@ const ARTIST_INPUT = document.getElementById("artist-input");
 const SONG_NAME_INPUT = document.getElementById("song-name-input");
 const TEMPO_INPUT = document.getElementById("tempo-input");
 const OUTPUT_TEXTAREA = document.getElementById("output-textarea");
+const ERROR_MESSAGE = document.getElementById("error-message");
 const SAVE_METADATA_BUTTON = document.getElementById("save-metadata-btn");
 const PLAY_HARMONY_BUTTON = document.getElementById("play-harmony-btn");
-const RESET_HARMONY_BUTTON = document.getElementById("reset-harmony-btn");
+const LOAD_HARMONY_BUTTON = document.getElementById("load-harmony-btn");
 const APPEND_HARMONY_BUTTON = document.getElementById("append-harmony-btn");
 const REMOVE_LAST_BUTTON = document.getElementById("remove-last-btn");
+const DOWNLOAD_BUTTON = document.getElementById("download-btn");
+const ADD_TO_LIBRARY_BUTTON = document.getElementById("add-to-library-btn");
 const STRING_ONE_SELECTOR = document.getElementById("string-one-selector");
 const STRING_TWO_SELECTOR = document.getElementById("string-two-selector");
 const STRING_THREE_SELECTOR = document.getElementById("string-three-selector");
@@ -17,6 +20,9 @@ const NOTE_LENGTH_SELECTOR = document.getElementById("note-length-selector");
 
 let output = "";
 let metadataExist = false;
+let currentMeasureLength = 0;
+// array containing previous harmony, elements 0-5 are strings 1-6, element 6 is note length
+let previousHarmony = ["X", "X", "X", "X", "X", "X", "01"];
 
 SAVE_METADATA_BUTTON.addEventListener("click", function() {
   saveMetadata();
@@ -26,8 +32,8 @@ PLAY_HARMONY_BUTTON.addEventListener("click", function() {
   playHarmony();
 });
 
-RESET_HARMONY_BUTTON.addEventListener("click", function() {
-  resetHarmony();
+LOAD_HARMONY_BUTTON.addEventListener("click", function() {
+  loadLastHarmony();
 });
 
 APPEND_HARMONY_BUTTON.addEventListener("click", function() {
@@ -36,42 +42,50 @@ APPEND_HARMONY_BUTTON.addEventListener("click", function() {
 REMOVE_LAST_BUTTON.addEventListener("click", function() {
   removeLastHarmony();
 });
+DOWNLOAD_BUTTON.addEventListener("click", function() {
+  downloadTab();
+});
+ADD_TO_LIBRARY_BUTTON.addEventListener("click", function() {
+  addToLibrary();
+});
 
 // replace first three rows in output data and textarea
 function saveMetadata() {
-  let lines = output.split("\n");
-
-  if (ARTIST_INPUT.value != "") {
-    lines[0] = ARTIST_INPUT.value;
-  } else {
-    lines[0] = "Umělec neuveden";
-  }
-
-  if (SONG_NAME_INPUT.value != "") {
-    lines[1] = SONG_NAME_INPUT.value;
-  } else {
-    lines[1] = "Název skladby neuveden";
-  }
-
   if (TEMPO_INPUT.value >= 40 && TEMPO_INPUT.value <= 240) {
+    ERROR_MESSAGE.innerText = "";
+    let lines = output.split("\n");
+
+    if (ARTIST_INPUT.value != "") {
+      lines[0] = ARTIST_INPUT.value;
+    } else {
+      lines[0] = "Umělec neuveden";
+    }
+
+    if (SONG_NAME_INPUT.value != "") {
+      lines[1] = SONG_NAME_INPUT.value;
+    } else {
+      lines[1] = "Název skladby neuveden";
+    }
+
     lines[2] = TEMPO_INPUT.value;
+
+    output = "";
+
+    for(let i = 0; i < lines.length; i++) {
+      output += lines[i] + "\n";
+    }
+
+    OUTPUT_TEXTAREA.innerHTML = output.replace("\n", "\r\n"); // HTML textarea requires \r\n for new line
+    metadataExist = true;
   } else {
-    lines[2] = "Tempo mimo rozsah";
+    ERROR_MESSAGE.innerText = "Error: Tempo mimo rozsah.";
   }
-
-  output = "";
-
-  for(let i = 0; i < lines.length; i++) {
-    output += lines[i] + "\n";
-  }
-
-  OUTPUT_TEXTAREA.innerHTML = output.replace("\n", "\r\n"); // HTML textarea requires \r\n for new line
-  metadataExist = true;
 }
 
 // play note and update string selector
 function selectNote(note) {
   if(TEMPO_INPUT.value >= 40 && TEMPO_INPUT.value <= 240) {
+    ERROR_MESSAGE.innerText = "";
     let length = ((60/TEMPO_INPUT.value)/(NOTE_LENGTH_SELECTOR.value/4));
     let audio = new Audio(`/sounds/${note}.wav`);
     audio.play();
@@ -81,13 +95,12 @@ function selectNote(note) {
       audio.currentTime = 0;
     }, length*1000);
   } else {
-    console.log("Tempo out of range.");
+    ERROR_MESSAGE.innerText = "Error: Tempo mimo rozsah.";
   }
 
   switch (note.substring(0, 1)) {
     case "1":
       STRING_ONE_SELECTOR.value = note.substring(2);
-      console.log(STRING_ONE_SELECTOR.value);
       break;
     case "2":
       STRING_TWO_SELECTOR.value = note.substring(2);
@@ -109,6 +122,7 @@ function selectNote(note) {
 
 function playHarmony() {
   if(TEMPO_INPUT.value >= 40 && TEMPO_INPUT.value <= 240) {
+    ERROR_MESSAGE.innerText = "";
     let length = ((60/TEMPO_INPUT.value)/(NOTE_LENGTH_SELECTOR.value/4));
     let audios = getAudioFiles();
     audios.forEach(audio => {
@@ -121,7 +135,7 @@ function playHarmony() {
         });
     }, length*1000);
   } else {
-    console.log("Tempo out of range.");
+    ERROR_MESSAGE.innerText = "Error: Tempo mimo rozsah.";
   }
 }
 
@@ -159,6 +173,7 @@ function getStringInfo() {
   return notes;
 }
 
+// reset harmony selectors
 function resetHarmony() {
   STRING_ONE_SELECTOR.value = "X";
   STRING_TWO_SELECTOR.value = "X";
@@ -168,31 +183,60 @@ function resetHarmony() {
   STRING_SIX_SELECTOR.value = "X";
 }
 
-function appendHarmony() {
-  let noteList = getStringInfo();
-  let notes = ":" + NOTE_LENGTH_SELECTOR.value;
-
-  if (!metadataExist) {
-    saveMetadata();
-  }
-
-  if(noteList.length == 0) {
-    notes += " XXXX";
+// check if measurement length is not too long or append | symbol if it equals 1
+function measureManagement() {
+  if (currentMeasureLength == 1) {
+    currentMeasureLength = 0;
+    output += "|\n"; 
+    OUTPUT_TEXTAREA.innerHTML += "|\r\n";
+    return 1;
+  } else if (currentMeasureLength + (1 / NOTE_LENGTH_SELECTOR.value) <= 1) {
+    return 1;
   } else {
-    noteList.forEach(note => {
-      notes += " " + note;
-    });
+    return 0;
   }
-  
-  output += notes + "\n";
-  OUTPUT_TEXTAREA.innerHTML = output.replace("\n", "\r\n"); // HTML textarea requires \r\n for new line
 }
 
+function appendHarmony() {
+  if (measureManagement()) {
+    ERROR_MESSAGE.innerText = "";
+    currentMeasureLength += 1 / NOTE_LENGTH_SELECTOR.value;
+
+    let noteList = getStringInfo();
+    let notes = ":" + NOTE_LENGTH_SELECTOR.value;
+
+    if (!metadataExist) {
+      saveMetadata();
+    }
+
+    if(noteList.length == 0) {
+      notes += " XXXX";
+    } else {
+      noteList.forEach(note => {
+        notes += " " + note;
+      });
+    }
+    
+    output += notes + "\n";
+    OUTPUT_TEXTAREA.innerHTML = output.replace("\n", "\r\n"); // HTML textarea requires \r\n for new line
+  } else {
+    ERROR_MESSAGE.innerText = "Délka not v tomto taktu neodpovídá 4/4 taktu.";
+  }
+  saveHarmony();
+  resetHarmony();
+}
+
+// remove last row
 function removeLastHarmony() {
   let lines = output.split("\n");
 
   if (lines.length > 4) {
     output = "";
+    if (lines[lines.length - 2].length > 7) { // if the row includes harmony
+      currentMeasureLength -= 1 / lines[lines.length - 2].substring(1,3);
+    } else { // reset measure length when removing | symbol
+      currentMeasureLength = 1;
+    }
 
     for(let i = 0; i < (lines.length - 2); i++) {
       output += lines[i] + "\n";
@@ -200,4 +244,69 @@ function removeLastHarmony() {
 
     OUTPUT_TEXTAREA.innerHTML = output.replace("\n", "\r\n"); // HTML textarea requires \r\n for new line
   }
+}
+
+function saveHarmony() {
+  previousHarmony[0] = STRING_ONE_SELECTOR.value;
+  previousHarmony[1] = STRING_TWO_SELECTOR.value;
+  previousHarmony[2] = STRING_THREE_SELECTOR.value;
+  previousHarmony[3] = STRING_FOUR_SELECTOR.value;
+  previousHarmony[4] = STRING_FIVE_SELECTOR.value;
+  previousHarmony[5] = STRING_SIX_SELECTOR.value;
+  previousHarmony[6] = NOTE_LENGTH_SELECTOR.value;
+}
+
+function loadLastHarmony() {
+  STRING_ONE_SELECTOR.value = previousHarmony[0];
+  STRING_TWO_SELECTOR.value = previousHarmony[1];
+  STRING_THREE_SELECTOR.value = previousHarmony[2];
+  STRING_FOUR_SELECTOR.value = previousHarmony[3];
+  STRING_FIVE_SELECTOR.value = previousHarmony[4];
+  STRING_SIX_SELECTOR.value = previousHarmony[5];
+  NOTE_LENGTH_SELECTOR.value = previousHarmony[6];
+}
+
+// prepare output data for export without affecting the current data
+function prepareForExport() {
+  let lines = output.split("\n");
+  let realOutput = "";
+
+  if (lines[lines.length - 2] != "|") {
+    realOutput = output + "|";
+    console.log("bez carky " + lines[lines.length - 2]);
+  }
+  else {
+    console.log("S carkou");
+    for(let i = 0; i < lines.length; i++) {
+      if (i < lines.length - 2) {
+        realOutput += lines[i] + "\n";
+      } else {
+        realOutput += lines[i];
+      }
+    }
+  }
+  return realOutput;
+}
+
+function downloadTab() {
+  let realOutput = prepareForExport();
+
+  let filename = "tab.txt";
+  let element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(realOutput));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+// TODO: finish this
+function addToLibrary() {
+  let realOutput = prepareForExport();
+
+  console.log(realOutput);
 }
